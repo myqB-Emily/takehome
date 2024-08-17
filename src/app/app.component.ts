12d858a0d685c499b5dac5991916b8a821563984
Shailesh Mishra <shaileshmishra@Shaileshs-MacBook-Pro.local>
2024-08-18 01:33:57 +0530

    device service integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { DeviceService } from './services/device.data.service';
-import { Observable, tap } from 'rxjs';
+import { combineLatest, concatAll, delay, filter, first, forkJoin, map, mergeMap, Observable, startWith, Subscription, switchMap, take, tap } from 'rxjs';
 import { IDevice } from './interfaces/device.interface';
+import { DeviceType } from './enums/device-types.enum';
+import { LightState } from './enums/light-states.enum';
+import { DoorState } from './enums/door-states.enum';
+import { DoorStateDisplayMap } from './constants/door-state-display-map.constants';
+import { LightStateDisplayMap } from './constants/light-state-display-map.constant';
 
 @Component({
   selector: 'app-root',
@@ -9,39 +14,66 @@ import { IDevice } from './interfaces/device.interface';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   device$: Observable<IDevice[]> | undefined;
+  devices: any[] = [];
+  loading = true;
+  private subscriptions: Subscription[] = [];
+  private deviceStateSubscriptions: Subscription[] = [];
+
   constructor(private readonly deviceService: DeviceService) {}
+ 
+  ngOnInit(): void {
+    this.loadDevices();
+    
+  }
 
-  /*
-  Given the available functions on the DeviceService, which works as an emulator for api 
-  calls, create a display grid of devices. The grid should display devices in a card
-  format with a border around them, they should be arranged left to right and overflow
-  onto the next line using modern css to help arrange the elements neatly.
+  private subscribeToDeviceStates(devices: any[]): void {
+    this.deviceStateSubscriptions = devices.map(device =>
+      this.deviceService.getDeviceStateDataStream(device).subscribe(state => {
+        const deviceToUpdate = this.devices.find(d => d.id === device.id);
+        if (deviceToUpdate) {
+          deviceToUpdate.state = state;
+        }
+      })
+    );
+  }
 
-  There is a load time to the get devices call, when the devices haven't been loaded
-  yet show a placeholder for loading.
-
-  You have a call available that gives you an observable stream that continually
-  gives you device state when you pass an individual device. Once you have your list of
-  devices displayed you should get the data stream for each device so you can actively 
-  update their states.
-
-  use the following methods:
+  loadDevices()
+  {
+    this.deviceService.getDevices().pipe(
+      switchMap(devices => {
+        this.devices = devices;
+        this.loading = false;
+        this.subscribeToDeviceStates(devices);
+        return [];
+      })
+    ).subscribe();
+  }
   
-  dataService.getDevices()  
-    //gives you an observable that gives you all of your devices
-  dataService.getDeviceStateDataStream() 
-    //gives you an observable event stream of a given device that continually outputs
-    state information
 
-  use the following constant files for your device state images:
-  constants/door-state-display-map.constants.ts
-  constants/light-state-display-map.constant.ts
 
-  fields that should be displayed in the device grid:
-  name
-  type
-  state (mapped to the image maps provided above)
-  */
+  //function to load the devices
+  loadDeviceTypeImg(state: any, deviceType: any)
+  {
+       if(deviceType === DeviceType.door)
+       {
+        return DoorStateDisplayMap[state]
+       }
+       else if(deviceType === DeviceType.light)
+       {
+         return LightStateDisplayMap[state]
+       }
+       else{
+         return 'N/A'
+       }
+      
+  }
+
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
+
+     
 }
--- src/app/constants/door-state-display-map.constants.ts
+++ src/app/constants/door-state-display-map.constants.ts
@@ -1,6 +1,6 @@
 import { DoorState } from '../enums/door-states.enum';
 
-export const DoorStateDisplayMap = {
+export const DoorStateDisplayMap:any = {
   [DoorState.closed]: 'https://placehold.co/60x60/000000/FFF?text=closed',
   [DoorState.opening]: 'https://placehold.co/60x60/acacac/FFF?text=opening',
   [DoorState.open]: 'https://placehold.co/60x60/3d7542/FFF?text=open',
